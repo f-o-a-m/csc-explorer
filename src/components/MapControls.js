4d130a6ca41597d7f994f9a59e0014146174ff4b
Gavin Atkinson <gatkinso@risd.edu>
2017-10-11 00:35:39 -0400

    Border-radius fix

--- src/components/MapControls.js
+++ src/components/MapControls.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MapControls = (props) => {
   return (
-    <footer id={'mapControlsContainer'} className={'shadowL'}>
+    <footer id={'mapControlsContainer'} className={'shadowL pill'}>
       <button className={'brl1 pillPad darkGrey'}>{'+'}</button>
       <button className={'darkGrey pillPad'}>{'-'}</button>
       <button className={'brr1 pillPad darkGrey'}>{'•'}</button>
