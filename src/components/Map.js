8faf04995343bcaf077a59ae8052a985cc6f25b7
Gavin Atkinson <gatkinso@risd.edu>
2017-10-31 15:05:36 -0400

    Fix bounding box

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,6 +10,10 @@ import Bubble from './Bubble'
 
 const TOKEN = 'pk.eyJ1IjoiY2FsbGlsIiwiYSI6ImNqN3V4eTVyazJqbWUzN25xdXNydzdrMXQifQ.Rsie4DpcanGTzTJgw8INWA'
 
+const metric = 'popularity'
+// a global threshold for displaying highlighted items
+const threshold = 0.85
+
 class Map extends Component {
   constructor(props) {
     super(props)
@@ -25,9 +29,6 @@ class Map extends Component {
     let res = Math.floor((z - 1) * 0.5) // geohash binning resolution
     let highlights = []
 
-    // a global threshold for displaying highlighted items
-    const metricThresh = 0.80
-
     // get a geohash at a resolution appropriate for zoom level
     const hasBins = coords.map(datum => {
       return Object.assign({...datum}, {bin: datum.geohash.substring(0, res)} )
@@ -39,10 +40,40 @@ class Map extends Component {
     // get the highest value in bin with respect to metric
     Object.keys(binned).forEach(key => {
       const max = lodash.maxBy(binned[key], metric)
-      max[metric] > metricThresh ? highlights.push(max) : null
+      highlights.push(max)
     })
 
     this.setState({highlights})
+  }
+
+  getProjectedViewportPolygon = (viewport, userWindow) => {
+    // bounding box
+    let bb = []
+    const win = {height: userWindow.innerHeight, width: userWindow.innerWidth}
+    const mercatorVP = new PerspectiveMercatorViewport(Object.assign({...viewport}, win))
+    // convert [x,y] in px to [lat, lng] with perspective
+    const topLeft = mercatorVP.unproject([1,1])
+    const topRight = mercatorVP.unproject([win.width - 1, 1])
+    const bottomRight = mercatorVP.unproject([win.width - 1, win.height - 1])
+    const bottomLeft = mercatorVP.unproject([1, win.height - 1])
+    // make an object that geolib.isPointInside expects
+    bb.push({latitude: topLeft[1], longitude: topLeft[0]})
+    bb.push({latitude: topRight[1], longitude: topRight[0]})
+    bb.push({latitude: bottomRight[1], longitude: bottomRight[0]})
+    bb.push({latitude: bottomLeft[1], longitude: bottomLeft[0]})
+    return bb
+  }
+
+  onViewportChange = (e, mapData) => {
+    // get items that are in viewport
+    const bb = this.getProjectedViewportPolygon(e, window)
+    const culled = mapData.filter(datum => {
+      let point = {latitude: datum.position[1], longitude: datum.position[0]}
+      return geolib.isPointInside(point, bb) && datum[metric] > threshold
+    })
+    this.getHighlights(culled, e, metric)
+    // tell redux
+    this.props.actions.onViewportChange(e)
   }
 
   renderMarkerList = (coords) => {
@@ -62,36 +93,6 @@ class Map extends Component {
     })
   }
 
-  onViewportChange = (e) => {
-    this.props.actions.onViewportChange(e)
-    // get items that are in viewport
-    const bb = this.getProjectedViewportBounds(e, window)
-    this.props.mapData.filter(datum => {
-      let point = {latitude: datum.position[1], longitude: datum.position[0]}
-      return geolib.isPointInside(point, bb)
-    })
-
-    // const bounds = this.mapRef.LngLatBounds()
-    // console.log(bounds)
-    this.getHighlights(this.props.mapData, this.props.viewport, 'popularity')
-  }
-
-  getProjectedViewportBounds = (viewport, userWindow) => {
-    let bb = []
-    const win = {height: userWindow.innerHeight, width: userWindow.innerWidth}
-    const mercatorVP = new PerspectiveMercatorViewport(Object.assign({...viewport}, win))
-    // get [lat, lng] at projected vp bounding box
-    const topLeft = mercatorVP.unproject([1,1])
-    const topRight = mercatorVP.unproject([1, win.width - 1])
-    const bottomRight = mercatorVP.unproject([win.height - 1, win.width - 1])
-    const bottomLeft = mercatorVP.unproject([win.height - 1, 1])
-    bb.push({latitude: topLeft[1], longitude: topLeft[0]})
-    bb.push({latitude: topRight[1], longitude: topRight[0]})
-    bb.push({latitude: bottomRight[1], longitude: bottomRight[0]})
-    bb.push({latitude: bottomLeft[1], longitude: bottomLeft[0]})
-    return bb
-  }
-
 
   render() {
     const props = this.props
@@ -100,7 +101,7 @@ class Map extends Component {
       <MapGL
         {...props.viewport}
         mapStyle={'mapbox://styles/mapbox/dark-v9'}
-        onViewportChange={(e) => this.onViewportChange(e)}
+        onViewportChange={(e) => this.onViewportChange(e, this.props.mapData)}
         ref={mapRef => this.mapRef = mapRef}
         mapboxApiAccessToken={TOKEN}>
         { this.state.highlights !== 0 ? this.renderMarkerList(this.state.highlights) : null }
